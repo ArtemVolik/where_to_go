944704320774e2b99303af8d8f9dc96abd980437
artyom <art.volyk@gmail.com>
2020-12-27 01:30:27 +0200

    env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, env.str('DB_NAME')),
+        'NAME': os.path.join(BASE_DIR, os.getenv('DB_NAME')),
     }
 }
 
