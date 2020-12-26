3a53c9d0f22eda80189f7fe802558524cb482152
artyom <art.volyk@gmail.com>
2020-12-27 01:19:18 +0200

    fix environment

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,19 +12,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 from environs import Env
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 env = Env()
 env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "REPLACE_ME")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -81,10 +85,11 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, env.str('DB_NAME')),
     }
 }
 
