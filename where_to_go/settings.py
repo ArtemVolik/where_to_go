6c23c5855e4b33a85c2ddc88a0f791916cfb8eda
artyom <art.volyk@gmail.com>
2020-12-27 01:37:16 +0200

    static_root

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -135,8 +135,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if DEBUG:
-        STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-    else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
 
