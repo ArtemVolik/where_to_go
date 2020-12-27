1178f8fc9c375cbed8a8fc1fce5ad8fdba7a1742
artyom <art.volyk@gmail.com>
2020-12-27 14:14:06 +0200

    static_root

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -130,12 +130,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = '/home/username/projects/site/assets/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'assets'),)
+STATIC_ROOT = 'static'
 
