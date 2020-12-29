1b85bf94235df70598bbba5085f35b27942c9ae6
artyom <art.volyk@gmail.com>
2020-12-29 02:33:39 +0200

    change method to update_or_create add defaults

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -14,14 +14,14 @@ def get_response(url) -> object:
 def save_place(place_properties) -> object:
     """ Save place in Db and return it as object."""
 
-    place = Place.objects.update_or_create(
+    place, _ = Place.objects.update_or_create(
         title=place_properties['title'],
         lon=place_properties['coordinates']['lng'],
         lat=place_properties['coordinates']['lat'],
         defaults={
             'short_description': place_properties['description_short'],
             'long_description': place_properties['description_long']}
-        )[0]
+        )
     place.save()
     return place
 
